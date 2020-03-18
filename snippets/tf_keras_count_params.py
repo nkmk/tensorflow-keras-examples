dbb43fc05f5a6d11391c7293d7b4fc0aa9e6dd08
nkmk <nkmk.on@gmail.com>
2020-03-18 18:41:12 +0900

    update count_params

--- snippets/tf_keras_count_params.py
+++ snippets/tf_keras_count_params.py
@@ -7,11 +7,12 @@ print(tf.__version__)
 tf.random.set_seed(0)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(1, (10, 10), padding='same',
-                           name='L0_conv2d', input_shape=(100, 100, 1)),
-    tf.keras.layers.Dense(10, name='L1_dense', use_bias=False),
-    tf.keras.layers.Dense(1, name='L2_dense'),
-    tf.keras.layers.BatchNormalization(name='L3_bn')
+    tf.keras.layers.Conv2D(1, (3, 3), padding='same',
+                           name='L0_conv2d', input_shape=(10, 10, 1)),
+    tf.keras.layers.Flatten(name='L1_flatten'),
+    tf.keras.layers.Dense(10, name='L2_dense', use_bias=False),
+    tf.keras.layers.Dense(1, name='L3_dense'),
+    tf.keras.layers.BatchNormalization(name='L4_bn')
 ])
 
 model.summary()
@@ -19,35 +20,37 @@ model.summary()
 # _________________________________________________________________
 # Layer (type)                 Output Shape              Param #   
 # =================================================================
-# L0_conv2d (Conv2D)           (None, 100, 100, 1)       101       
+# L0_conv2d (Conv2D)           (None, 10, 10, 1)         10        
 # _________________________________________________________________
-# L1_dense (Dense)             (None, 100, 100, 10)      10        
+# L1_flatten (Flatten)         (None, 100)               0         
 # _________________________________________________________________
-# L2_dense (Dense)             (None, 100, 100, 1)       11        
+# L2_dense (Dense)             (None, 10)                1000      
 # _________________________________________________________________
-# L3_bn (BatchNormalization)   (None, 100, 100, 1)       4         
+# L3_dense (Dense)             (None, 1)                 11        
+# _________________________________________________________________
+# L4_bn (BatchNormalization)   (None, 1)                 4         
 # =================================================================
-# Total params: 126
-# Trainable params: 124
+# Total params: 1,025
+# Trainable params: 1,023
 # Non-trainable params: 2
 # _________________________________________________________________
 
 print(model.count_params())
-# 126
+# 1025
 
 print(model.layers[0].count_params())
-# 101
+# 10
 
 print(issubclass(tf.keras.Model, tf.keras.layers.Layer))
 # True
 
-print(type(model.layers[2].get_weights()))
+print(type(model.layers[3].get_weights()))
 # <class 'list'>
 
-print(len(model.layers[2].get_weights()))
+print(len(model.layers[3].get_weights()))
 # 2
 
-kernel_weights, bias = model.layers[2].get_weights()
+kernel_weights, bias = model.layers[3].get_weights()
 
 print(kernel_weights)
 # [[-0.45019907]
@@ -76,7 +79,7 @@ print(kernel_weights.size)
 print(bias.size)
 # 1
 
-k_size, b_size = [w.size for w in model.layers[2].get_weights()]
+k_size, b_size = [w.size for w in model.layers[3].get_weights()]
 
 print(k_size)
 # 10
@@ -84,29 +87,13 @@ print(k_size)
 print(b_size)
 # 1
 
-print(type(model.layers[2].weights))
+print(type(model.layers[3].weights))
 # <class 'list'>
 
-print(len(model.layers[2].weights))
+print(len(model.layers[3].weights))
 # 2
 
-kernel_weights, bias = model.layers[2].weights
-
-print(kernel_weights)
-# <tf.Variable 'L2_dense/kernel:0' shape=(10, 1) dtype=float32, numpy=
-# array([[-0.45019907],
-#        [ 0.3547594 ],
-#        [-0.01801795],
-#        [ 0.5543849 ],
-#        [-0.13720274],
-#        [-0.71705985],
-#        [ 0.30951375],
-#        [-0.19865453],
-#        [ 0.11943179],
-#        [ 0.5920785 ]], dtype=float32)>
-
-print(bias)
-# <tf.Variable 'L2_dense/bias:0' shape=(1,) dtype=float32, numpy=array([0.], dtype=float32)>
+kernel_weights, bias = model.layers[3].weights
 
 print(type(kernel_weights))
 # <class 'tensorflow.python.ops.resource_variable_ops.ResourceVariable'>
@@ -115,7 +102,7 @@ print(issubclass(type(kernel_weights), tf.Variable))
 # True
 
 print(kernel_weights.name)
-# L2_dense/kernel:0
+# L3_dense/kernel:0
 
 print(kernel_weights.shape)
 # (10, 1)
@@ -148,16 +135,16 @@ print(type(model.weights[0]))
 # <class 'tensorflow.python.ops.resource_variable_ops.ResourceVariable'>
 
 for w in model.weights:
-    print(w.name, ' - ', np.prod(w.shape))
-# L0_conv2d/kernel:0  -  100
-# L0_conv2d/bias:0  -  1
-# L1_dense/kernel:0  -  10
-# L2_dense/kernel:0  -  10
-# L2_dense/bias:0  -  1
-# L3_bn/gamma:0  -  1
-# L3_bn/beta:0  -  1
-# L3_bn/moving_mean:0  -  1
-# L3_bn/moving_variance:0  -  1
+    print('{:<25}{:<15}{}'.format(w.name, str(w.shape), np.prod(w.shape)))
+# L0_conv2d/kernel:0       (3, 3, 1, 1)   9
+# L0_conv2d/bias:0         (1,)           1
+# L2_dense/kernel:0        (100, 10)      1000
+# L3_dense/kernel:0        (10, 1)        10
+# L3_dense/bias:0          (1,)           1
+# L4_bn/gamma:0            (1,)           1
+# L4_bn/beta:0             (1,)           1
+# L4_bn/moving_mean:0      (1,)           1
+# L4_bn/moving_variance:0  (1,)           1
 
 print(type(model.get_weights()))
 # <class 'list'>
@@ -170,9 +157,9 @@ print(type(model.get_weights()[0]))
 
 for w in model.get_weights():
     print(w.size)
-# 100
+# 9
 # 1
-# 10
+# 1000
 # 10
 # 1
 # 1
@@ -180,70 +167,72 @@ for w in model.get_weights():
 # 1
 # 1
 
-model.layers[1].trainable = False
+model.layers[2].trainable = False
 
 model.summary()
 # Model: "sequential"
 # _________________________________________________________________
 # Layer (type)                 Output Shape              Param #   
 # =================================================================
-# L0_conv2d (Conv2D)           (None, 100, 100, 1)       101       
+# L0_conv2d (Conv2D)           (None, 10, 10, 1)         10        
 # _________________________________________________________________
-# L1_dense (Dense)             (None, 100, 100, 10)      10        
+# L1_flatten (Flatten)         (None, 100)               0         
 # _________________________________________________________________
-# L2_dense (Dense)             (None, 100, 100, 1)       11        
+# L2_dense (Dense)             (None, 10)                1000      
 # _________________________________________________________________
-# L3_bn (BatchNormalization)   (None, 100, 100, 1)       4         
+# L3_dense (Dense)             (None, 1)                 11        
+# _________________________________________________________________
+# L4_bn (BatchNormalization)   (None, 1)                 4         
 # =================================================================
-# Total params: 126
-# Trainable params: 114
-# Non-trainable params: 12
+# Total params: 1,025
+# Trainable params: 23
+# Non-trainable params: 1,002
 # _________________________________________________________________
-
-print(model.layers[1].trainable)
-# False
-
-print(model.layers[1].trainable_weights)
-# []
-
-print(model.layers[1].non_trainable_weights == model.layers[1].weights)
-# True
 
 print(model.layers[2].trainable)
-# True
+# False
 
-print(model.layers[2].non_trainable_weights)
+print(model.layers[2].trainable_weights)
 # []
 
-print(model.layers[2].trainable_weights == model.layers[2].weights)
+print(model.layers[2].non_trainable_weights == model.layers[2].weights)
 # True
-
-trainable_params = sum(np.prod(w.shape) for w in model.trainable_weights)
-print(trainable_params)
-# 114
-
-non_trainable_params = sum(np.prod(w.shape) for w in model.non_trainable_weights)
-print(non_trainable_params)
-# 12
 
 print(model.layers[3].trainable)
 # True
 
-for w in model.layers[3].trainable_weights:
-    print(w.name, ' - ', np.prod(w.shape))
-# L3_bn/gamma:0  -  1
-# L3_bn/beta:0  -  1
+print(model.layers[3].non_trainable_weights)
+# []
 
-for w in model.layers[3].non_trainable_weights:
-    print(w.name, ' - ', np.prod(w.shape))
-# L3_bn/moving_mean:0  -  1
-# L3_bn/moving_variance:0  -  1
+print(model.layers[3].trainable_weights == model.layers[3].weights)
+# True
+
+trainable_params = sum(np.prod(w.shape) for w in model.trainable_weights)
+print(trainable_params)
+# 23
+
+non_trainable_params = sum(np.prod(w.shape) for w in model.non_trainable_weights)
+print(non_trainable_params)
+# 1002
+
+print(model.layers[4].trainable)
+# True
+
+for w in model.layers[4].trainable_weights:
+    print('{:<25}{}'.format(w.name, np.prod(w.shape)))
+# L4_bn/gamma:0            1
+# L4_bn/beta:0             1
+
+for w in model.layers[4].non_trainable_weights:
+    print('{:<25}{}'.format(w.name, np.prod(w.shape)))
+# L4_bn/moving_mean:0      1
+# L4_bn/moving_variance:0  1
 
 # NG
 trainable_params = sum(l.count_params() for l in model.layers if l.trainable)
 print(trainable_params)
-# 116
+# 25
 
 trainable_params = sum(l.count_params() for l in model.layers if not l.trainable)
 print(trainable_params)
-# 10
+# 1000
